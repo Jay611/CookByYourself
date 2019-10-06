cc693cd868ac02811253585798e19dfb5b8ca53b
Jay611 <nanbaejjae@gmail.com>
2019-10-06 13:59:11 -0400

    revised Home.cshtml

---
+++ CookByYourself/Controllers/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CookByYourself.Models;
+
+namespace CookByYourself.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public ViewResult Home() => View();
+
+        public ViewResult DisplayPage() => View();
+
+        public ViewResult DataPage() => View();
+
+        public ViewResult InsertPage() => View();
+
+        public ViewResult UserPage() => View();
+    }
+    
+}
---
+++ CookByYourself/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookByYourself.Models
+{
+    public class Recipe
+    {
+        public int RecipeID { get; set; }
+        public string Name { get; set; }
+    }
+}
